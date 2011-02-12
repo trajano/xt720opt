b763d3ad177f0f6b8a13fb1cbd08d9508aebc256
Archimedes Trajano <arch@trajano.net>
2011-02-12 00:56:34 -0500

    copied busybox to the base folder
    The install-update will now use the busybox that comes in the base
    folder.  However, it does not install busybox.

--- install-update.sh
+++ install-update.sh
@@ -5,10 +5,9 @@ UPDATED_FILES=""
 
 # sets up backup folders
 BACKUP_DIR=/sdcard/xt720opt.backup/`date +"%Y-%m-%dT%H.%M.%S"`
-busybox mkdir -p $BACKUP_DIR
+./busybox mkdir -p $BACKUP_DIR
 
-SYS_PARTITION=`mount | grep "^.* /system " | awk ' { print $1 } '`
-
+SYS_PARTITION=`mount | ./busybox grep "^.* /system " | ./busybox awk ' { print $1 } '`
 
 cat_cp() {
    dest=`echo "$1" | sed 's#^pkg[^/]*##'`
@@ -31,6 +30,51 @@ cat_cp() {
    fi
 }
 
+do_update() {
+   for file in `find pkg -type f -print`
+   do
+      cat_cp $file
+      chmod 644 $file
+   done
+   for file in `find pkgexec -type f -print`
+   do
+      cat_cp $file
+      chmod 755 $file
+   done
+   
+   # Update files that should only be updated in OpenRecovery mode
+   if [ "$INIT_DIR" = "/sdcard/OpenRecovery/init" ]
+   then
+      echo "In Openrecovery mode, updating pkgrecovery files"
+      for file in `find pkgrecovery -type f -print`
+      do
+         cat_cp $file
+         chmod 644 $file
+      done
+      for file in `find pkgrecoveryexec -type f -print`
+      do
+         cat_cp $file
+         chmod 755 $file
+      done
+   fi
+   
+   # TODO some future updates I may want to delete the files in which case I will put the code here.
+   
+   # Reinstall busybox if updated
+   if echo $UPDATED_FILES | grep /system/xbin/busybox
+   then
+      /system/xbin/busybox --install -s /system/xbin/
+   fi
+   
+   # Remove dalvik-cache if dexopt or dalvik vm is updated
+   if (echo $UPDATED_FILES | grep -q /system/bin/davikvm) || (echo $UPDATED_FILES | grep -q /system/bin/dexopt)
+   then
+      [ -e /system/sd/dalvik-cache ] && rm -r /system/sd/dalvik-cache/*
+      [ -e /sddata/dalvik-cache ] && rm -r /sddata/dalvik-cache/*
+      [ -e /data/dalvik-cache ] && rm -r /data/dalvik-cache/*
+   fi
+}
+
 # Remount system partion as read-write
 mount -t yaffs2 -o rw,remount $SYS_PARTITION /system
 
@@ -42,49 +86,6 @@ then
    then
       mv /system/bin/mot_boot_mode.bin /system/bin/mot_boot_mode
    fi
-fi
-
-for file in `find pkg -type f -print`
-do
-   cat_cp $file
-   chmod 644 $file
-done
-for file in `find pkgexec -type f -print`
-do
-   cat_cp $file
-   chmod 755 $file
-done
-
-# Update files that should only be updated in OpenRecovery mode
-if [ "$INIT_DIR" = "/sdcard/OpenRecovery/init" ]
-then
-   echo "In Openrecovery mode, updating pkgrecovery files"
-   for file in `find pkgrecovery -type f -print`
-   do
-      cat_cp $file
-      chmod 644 $file
-   done
-   for file in `find pkgrecoveryexec -type f -print`
-   do
-      cat_cp $file
-      chmod 755 $file
-   done
-fi
-
-# TODO some future updates I may want to delete the files in which case I will put the code here.
-
-# Reinstall busybox if updated
-if echo $UPDATED_FILES | grep /system/xbin/busybox
-then
-   /system/xbin/busybox --install -s /system/xbin/
-fi
-
-# Remove dalvik-cache if dexopt or dalvik vm is updated
-if (echo $UPDATED_FILES | grep -q /system/bin/davikvm) || (echo $UPDATED_FILES | grep -q /system/bin/dexopt)
-then
-   [ -e /system/sd/dalvik-cache ] && rm -r /system/sd/dalvik-cache/*
-   [ -e /sddata/dalvik-cache ] && rm -r /sddata/dalvik-cache/*
-   [ -e /data/dalvik-cache ] && rm -r /data/dalvik-cache/*
 fi
 
 # Prevent any file loss at this time
